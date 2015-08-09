662d0d45a5fcd31a952762453bf9b997841e61d5
user nowhere <user@nowhere.com>
2015-08-09 15:29:02 -0500

    Getting things ready for the big switchover

--- server/boot/explorer.js
+++ server/boot/explorer.js
@@ -1,21 +1,21 @@
+'use strict';
+
 module.exports = function mountLoopBackExplorer(server) {
   var explorer;
   try {
     explorer = require('loopback-explorer');
-  } catch(err) {
-    // Print the message only when the app was started via `server.listen()`.
-    // Do not print any message when the project is used as a component.
-    server.once('started', function(baseUrl) {
-      console.log(
-        'Run `npm install loopback-explorer` to enable the LoopBack explorer'
-      );
-    });
+  } catch (err) {
+    console.log(
+      'Run `npm install loopback-explorer` to enable the LoopBack explorer'
+    );
     return;
   }
 
   var restApiRoot = server.get('restApiRoot');
 
-  var explorerApp = explorer(server, { basePath: restApiRoot });
+  var explorerApp = explorer(server, {
+    basePath: restApiRoot
+  });
   server.use('/explorer', explorerApp);
   server.once('started', function() {
     var baseUrl = server.get('url').replace(/\/$/, '');
--- server/server.js
+++ server/server.js
@@ -1,18 +1,59 @@
+'use strict';
 /*
-var mongoAddr = '"' + process.env.MONGO_PORT_27017_TCP_ADDR + '"';
-var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT;
-var fs = require('fs');
+ var mongoAddr = '"' + process.env.MONGO_PORT_27017_TCP_ADDR + '"';
+ var mongoPort = process.env.MONGO_PORT_27017_TCP_PORT;
+ var fs = require('fs');
 
-var fileContents = fs.readFileSync(__dirname + '/datasources_template.json', 'utf8');
-fileContents = fileContents.replace(/MONGO_HOST_ADDR/g, mongoAddr);
-fileContents = fileContents.replace(/MONGO_HOST_PORT/g, mongoPort);
-fs.writeFileSync(__dirname + '/datasources.json', fileContents);
-*/
-
+ var fileContents = fs.readFileSync(__dirname + '/datasources_template.json', 'utf8');
+ fileContents = fileContents.replace(/MONGO_HOST_ADDR/g, mongoAddr);
+ fileContents = fileContents.replace(/MONGO_HOST_PORT/g, mongoPort);
+ fs.writeFileSync(__dirname + '/datasources.json', fileContents);
+ */
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
+var path = require('path');
 var app = module.exports = loopback();
+var env = require('get-env')({
+  test: 'test'
+});
+
+// Set up the /favicon.ico
+app.use(loopback.favicon());
+
+// request pre-processing middleware
+app.use(loopback.compress());
+
+// -- Add your pre-processing middleware here --
+
+
+// boot scripts mount components like REST API
+boot(app, __dirname);
+
+
+// -- Mount static files here--
+// All static middleware should be registered at the end, as all requests
+// passing the static middleware are hitting the file system
+// Example:
+
+var staticPath = null;
+
+if (env !== 'prod') {
+  staticPath = path.resolve(__dirname, '../client/app/');
+  console.log("Running app in development mode");
+} else {
+  staticPath = path.resolve(__dirname, '../dist/');
+  console.log("Running app in prodction mode");
+}
+
+app.use(loopback.static(staticPath));
+
+// Requests that get this far won't be handled
+// by any middleware. Convert them into a 404 error
+// that will be handled later down the chain.
+app.use(loopback.urlNotFound());
+
+// The ultimate error handler.
+app.use(loopback.errorHandler());
 
 app.start = function() {
   // start the web server
@@ -22,12 +63,7 @@ app.start = function() {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
